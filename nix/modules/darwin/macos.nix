bc1237493a345cf8cfed82f1af5370c33b955baf
ketal <k@ketal.icu>
2026-05-12 14:33:26 +0800

    feat(darwin): tune macos defaults

--- nix/modules/darwin/macos.nix
+++ nix/modules/darwin/macos.nix
@@ -1,3 +1,8 @@
+{ username, ... }:
+
+let
+  byHostPreferences = "~${username}/Library/Preferences/ByHost";
+in
 {
   # macOS defaults managed by nix-darwin
   system.defaults = {
@@ -37,6 +42,9 @@
       # Dock: automatically hide and show the Dock.
       autohide = true;
 
+      # Mission Control: group windows by application.
+      expose-group-apps = true;
+
       # Dock: hide suggested and recent apps.
       show-recents = false;
     };
@@ -45,11 +53,27 @@
       # Global: show all filename extensions in apps that honor the global preference.
       AppleShowAllExtensions = true;
 
+      # Menu Bar: reduce horizontal spacing between status icons.
+      NSStatusItemSpacing = 6;
+
+      # Menu Bar: reduce clickable padding around status icons.
+      NSStatusItemSelectionPadding = 4;
+
       # Keyboard: use F1, F2, etc. keys as standard function keys.
       "com.apple.keyboard.fnState" = true;
     };
 
     CustomUserPreferences = {
+      "${byHostPreferences}/com.apple.Spotlight" = {
+        # Menu Bar: hide the Spotlight icon.
+        MenuItemHidden = 1;
+      };
+
+      "com.apple.controlcenter" = {
+        # Menu Bar: hide the Keyboard Brightness icon.
+        "NSStatusItem Visible KeyboardBrightness" = false;
+      };
+
       "com.apple.desktopservices" = {
         # Desktop Services: avoid creating .DS_Store files on network volumes.
         DSDontWriteNetworkStores = true;
